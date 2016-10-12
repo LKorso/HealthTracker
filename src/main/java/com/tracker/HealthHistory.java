1c7d054859335a475996e9a065948df0266bef9c
Dmytro Skrypnyk <Dmytro_Skrypnyk@epam.com>
2016-10-12 13:49:01 +0300

    Add class HealthHistory

---
+++ src/main/java/com/tracker/HealthHistory.java
@@ -0,0 +1,14 @@
+package com.tracker;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HealthHistory {
+    private Map<Date, Integer> caloriesHistory = new HashMap<Date, Integer>();
+    private Map<Date, Integer> stepsHistory = new HashMap<Date, Integer>();
+    private Map<Date, Integer> waterVolumeHistory = new HashMap<Date, Integer>();
+    private Map<Date, Integer> walkingInMInutesHistory = new HashMap<Date, Integer>();
+
+    
+}
